589ef92e3630abce3dd70b0e98b4a0177d576510
liupoyip <liupoyip@gmail.com>
2023-11-02 13:54:00 +0800

    LGTM!!

--- GUI/pyqt6/chartview/wave_plot/chart.py
+++ GUI/pyqt6/chartview/wave_plot/chart.py
@@ -8,18 +8,18 @@ from PySide6.QtGui import QPen, QColor, QPalette, QBrush, QWheelEvent, qRgb, QMo
 
 
 class ChartView(QChartView):
-    _vertical_line_x = None
+    # vertical_line_x = None
     # _vertical_line_pen = QPen(QColor('red'))
     # _vertical_line_pen.setWidth(2)
+    vertical_line_x = None
+    # @property
+    # def vertical_line_x(self):
+    #     return self._vertical_line_x
 
-    @property
-    def vertical_line_x(self):
-        return self._vertical_line_x
-
-    @vertical_line_x.setter
-    def vertical_line_x(self, vertical_line_x):
-        self._vertical_line_x = vertical_line_x
-        self.update()
+    # @vertical_line_x.setter
+    # def vertical_line_x(self, vertical_line_x):
+    #     self._vertical_line_x = vertical_line_x
+    #     self.update()
 
     def drawForeground(self, painter, rect):
         if self.vertical_line_x is None:
@@ -42,33 +42,33 @@ class LineChart(QWidget):
 
     def __init__(self):
         super().__init__()
-        self._chart = QChart()
-        self._series = QLineSeries()
+        self.chart = QChart()
+        self.series = QLineSeries()
         self.PushButton = QPushButton()
 
-        self._chart.addSeries(self._series)
-        self._axis_x = QValueAxis()
-        self._axis_x.setRange(0, 100)
-        self._axis_x.setGridLineVisible(False)
+        self.chart.addSeries(self.series)
+        self.axis_x = QValueAxis()
+        self.axis_x.setRange(0, 100)
+        self.axis_x.setGridLineVisible(False)
 
-        self._axis_y = QValueAxis()
-        self._axis_y.setRange(-1, 1)
-        self._axis_y.setGridLineVisible(True)
+        self.axis_y = QValueAxis()
+        self.axis_y.setRange(-1, 1)
+        self.axis_y.setGridLineVisible(True)
 
-        self._chart.addAxis(self._axis_x, Qt.AlignBottom)
-        self._chart.addAxis(self._axis_y, Qt.AlignLeft)
-        self._series.attachAxis(self._axis_x)
-        self._series.attachAxis(self._axis_y)
-        self._chart.legend().hide()
+        self.chart.addAxis(self.axis_x, Qt.AlignBottom)
+        self.chart.addAxis(self.axis_y, Qt.AlignLeft)
+        self.series.attachAxis(self.axis_x)
+        self.series.attachAxis(self.axis_y)
+        self.chart.legend().hide()
 
-        self._chart.setBackgroundRoundness(0)
-        self._chart.layout().setContentsMargins(0, 0, 0, 0)
+        self.chart.setBackgroundRoundness(0)
+        self.chart.layout().setContentsMargins(0, 0, 0, 0)
         self.chart_view = ChartView()
-        self.chart_view.setChart(self._chart)
-        self._x = np.linspace(0, 100, 512)
-        self._y = np.zeros(512)
-        self._buffer = [QPointF(x, y) for x, y in zip(self._x, self._y)]
-        self._series.append(self._buffer)
+        self.chart_view.setChart(self.chart)
+        self.line_x = np.linspace(0, 100, 512)
+        self.line_y = np.zeros(512)
+        self.buffer = [QPointF(x, y) for x, y in zip(self.line_x, self.line_y)]
+        self.series.append(self.buffer)
         self.set_dark_theme()
 
     def mouseMoveEvent(self, event) -> None:
@@ -78,100 +78,100 @@ class LineChart(QWidget):
         # return QChartView.mouseMoveEvent(self, event)
 
     def set_y(self, y_line):
-        if len(self._buffer) != y_line.shape[0]:
+        if len(self.buffer) != y_line.shape[0]:
             raise BaseException('Length of buffer and y-line is not match!')
         for i in range(y_line.shape[0]):
-            self._buffer[i].setY(y_line[i])
-        self._series.replace(self._buffer)
+            self.buffer[i].setY(y_line[i])
+        self.series.replace(self.buffer)
 
     def set_dark_theme(self):
-        self._pen = QPen()
-        self._pen.setWidth(1)
-        self._pen.setColor(QColor(Qt.cyan))
-        self._series.setPen(self._pen)
+        self.pen = QPen()
+        self.pen.setWidth(1)
+        self.pen.setColor(QColor(Qt.cyan))
+        self.series.setPen(self.pen)
 
-        self._chart.setBackgroundBrush(QBrush(QColor(qRgb(35, 35, 45))))
-        self._axis_x.setTitleBrush(QBrush(QColor(Qt.white)))
-        self._axis_y.setTitleBrush(QBrush(QColor(Qt.white)))
-        self._axis_x.setLabelsBrush(QBrush(QColor(Qt.white)))
-        self._axis_y.setLabelsBrush(QBrush(QColor(Qt.white)))
+        self.chart.setBackgroundBrush(QBrush(QColor(qRgb(35, 35, 45))))
+        self.axis_x.setTitleBrush(QBrush(QColor(Qt.white)))
+        self.axis_y.setTitleBrush(QBrush(QColor(Qt.white)))
+        self.axis_x.setLabelsBrush(QBrush(QColor(Qt.white)))
+        self.axis_y.setLabelsBrush(QBrush(QColor(Qt.white)))
 
 
 class WaveChart(LineChart):
     def __init__(self):
         super(WaveChart, self).__init__()
 
-        self._axis_x.setRange(0, 100)
-        self._axis_x.setLabelFormat('%d')
-        self._axis_x.setLabelsEditable(True)
-        self._axis_x.setTitleText("time (ms)")
-        # self._axis_x.setTitleVisible(False)
-        self._axis_x.setGridLineVisible(False)
+        self.axis_x.setRange(0, 100)
+        self.axis_x.setLabelFormat('%d')
+        self.axis_x.setLabelsEditable(True)
+        self.axis_x.setTitleText("time (ms)")
+        # self.axis_x.setTitleVisible(False)
+        self.axis_x.setGridLineVisible(False)
 
-        self._axis_y.setRange(-0.1, 0.1)
-        self._axis_y.setLabelFormat('%.2f')
-        self._axis_y.setLabelsEditable(True)
-        self._axis_y.setTitleText("value")
-        # self._axis_y.setTitleVisible(False)
-        self._axis_y.setGridLineVisible(True)
+        self.axis_y.setRange(-0.1, 0.1)
+        self.axis_y.setLabelFormat('%.2f')
+        self.axis_y.setLabelsEditable(True)
+        self.axis_y.setTitleText("value")
+        # self.axis_y.setTitleVisible(False)
+        self.axis_y.setGridLineVisible(True)
 
-        self._x = np.linspace(0, 100, 512)
-        self._y = np.zeros(512)
-        self._buffer = [QPointF(x, y) for x, y in zip(self._x, self._y)]
-        self._series.append(self._buffer)
+        self.line_x = np.linspace(0, 100, 512)
+        self.line_y = np.zeros(512)
+        self.buffer = [QPointF(x, y) for x, y in zip(self.line_x, self.line_y)]
+        self.series.append(self.buffer)
 
         # self.mouse_moved = Signal(QPoint)
         # self.mouse_moved.connect(self.mouseMoveEvent)
 
     def reset_axis(self, end_point, buffer_len):
-        self._axis_x.setRange(0, end_point)
-        self._x = np.linspace(0, end_point, buffer_len)
-        self._y = np.zeros(buffer_len)
-        self._buffer = [QPointF(x, y) for x, y in zip(self._x, self._y)]
-        self._series.replace(self._buffer)
+        self.axis_x.setRange(0, end_point)
+        self.line_x = np.linspace(0, end_point, buffer_len)
+        self.line_y = np.zeros(buffer_len)
+        self.buffer = [QPointF(x, y) for x, y in zip(self.line_x, self.line_y)]
+        self.series.replace(self.buffer)
 
     def set_y_range(self, low_limit: float, high_limit: float):
-        self._axis_y.setRange(low_limit, high_limit)
+        self.axis_y.setRange(low_limit, high_limit)
 
     def set_y_label(self, label: str):
-        self._axis_y.setTitleText(label)
+        self.axis_y.setTitleText(label)
 
 
 class SpectrumChart(LineChart):
     def __init__(self):
         super(SpectrumChart, self).__init__()
 
-        self._axis_x.setRange(0, 1600)
-        self._axis_x.setLabelFormat('%d')
-        self._axis_x.setLabelsEditable(True)
-        self._axis_x.setTitleText("Hz")
-        self._axis_x.setGridLineVisible(False)
-        self._axis_x.setTickCount(11)
+        self.axis_x.setRange(0, 1600)
+        self.axis_x.setLabelFormat('%d')
+        self.axis_x.setLabelsEditable(True)
+        self.axis_x.setTitleText("Hz")
+        self.axis_x.setGridLineVisible(False)
+        self.axis_x.setTickCount(11)
 
-        self._axis_y.setRange(0, 1)
-        self._axis_y.setLabelFormat('%.2f')
-        self._axis_y.setLabelsEditable(True)
-        self._axis_y.setTitleText("power (norm 0~1)")
+        self.axis_y.setRange(0, 1)
+        self.axis_y.setLabelFormat('%.2f')
+        self.axis_y.setLabelsEditable(True)
+        self.axis_y.setTitleText("power (norm 0~1)")
 
-        self._x = np.linspace(0, 1600, 512)
-        self._y = np.zeros(512)
+        self.line_x = np.linspace(0, 1600, 512)
+        self.line_y = np.zeros(512)
 
-        self._buffer = [QPointF(x, y) for x, y in zip(self._x, self._y)]
-        self._series.append(self._buffer)
+        self.buffer = [QPointF(x, y) for x, y in zip(self.line_x, self.line_y)]
+        self.series.append(self.buffer)
 
         # self.mouse_moved = Signal(QPoint)
         # self.mouse_moved.connect(self.mouseMoveEvent)
 
     def reset_axis(self, end_point, buffer_len):
-        self._axis_x.setRange(0, end_point)
-        self._axis_y.setRange(0, 1)
-        self._x = np.linspace(0, end_point, buffer_len)
-        self._y = np.zeros(buffer_len)
-        self._buffer = [QPointF(x, y) for x, y in zip(self._x, self._y)]
-        self._series.replace(self._buffer)
+        self.axis_x.setRange(0, end_point)
+        self.axis_y.setRange(0, 1)
+        self.line_x = np.linspace(0, end_point, buffer_len)
+        self.line_y = np.zeros(buffer_len)
+        self.buffer = [QPointF(x, y) for x, y in zip(self.line_x, self.line_y)]
+        self.series.replace(self.buffer)
 
     def set_x_range(self, low_limit: float, high_limit: float):
-        self._axis_x.setRange(low_limit, high_limit)
+        self.axis_x.setRange(low_limit, high_limit)
 
     def mark_max_x(self):
         ...
